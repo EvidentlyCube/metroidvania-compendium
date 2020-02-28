08c556c6e32fe84b45e57f47034bbcee84f86868
Michal Obirek <obirek999@gmail.com>
2020-02-28 18:27:56 +0100

    And little fix to delete esLint warning

--- frontend/Views/Home.tsx
+++ frontend/Views/Home.tsx
@@ -11,8 +11,8 @@ export class Home extends React.Component {
 				<div className="container">
 					<article>
 						<p>A comprehensive compendium of abilities in Metroidvania games. Perfect research material for your own game.
-                    	Excellent if you want to learn more about the genre you love.
-                    	Invaluable source of inspiration for any kind of game.</p>
+						Excellent if you want to learn more about the genre you love.
+						Invaluable source of inspiration for any kind of game.</p>
 						<h2>Beware spoilers!</h2>
 						<p><Link to="/config">Configure your experience</Link> to avoid them.</p>
 					</article>
