2aa496cf1cde756fff1132b83a9438041f4e4f6d
Michal Obirek <obirek999@gmail.com>
2020-03-09 15:05:32 +0100

    Changes to ConfigView to load data from Redux

---
+++ frontend/storage/reducers/breadcrumbReducer.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import {AppStore, SET_BREADCRUMB, AppActions} from '../common';
+
+export function breadcrumbReducer(state: AppStore, action: AppActions): AppStore {
+	switch (action.type) {
+		case SET_BREADCRUMB: {
+			return {
+				...state,
+				headerBreadcrumb: action.headerBreadcrumb || '',
+			};
+		}
+		default:
+			return state;
+	}
+}
