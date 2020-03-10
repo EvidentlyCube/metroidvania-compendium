e1f5e6eae85c58312c362b2a9d3ea78e1a557388
Michal Obirek <obirek999@gmail.com>
2020-03-10 21:18:12 +0100

    Styled components part 1

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import { Styles } from './styles';
 
-export const Footer = () => {
+export const Footer: React.FC = () => {
 	return (
-		<footer>
+		<Container>
 			<p>
 				Follow on <a href="https://github.com/EvidentlyCube/metroidvania-compendium">GitHub</a>
 			</p>
-		</footer>
+		</Container>
 	);
 };
+const Container = styled.footer`
+	display: flex;
+	justify-content: center;
+	padding: 10px;
+	background: ${Styles.colorDarkBg};
+	color: ${Styles.colorTextOnDarkBg};
+
+	a {
+		color: ${Styles.colorTextOnDarkBg};
+	}
+`;
--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { AppStore } from '../storage/common';
+import styled from 'styled-components';
+import { Styles } from './styles';
 
 interface HeaderProps {
 	breadcrumb: string;
@@ -10,7 +12,7 @@ const Header: React.FC<HeaderProps> = props => {
 	const { breadcrumb } = props;
 	return (
 		<>
-			<nav>
+			<Container>
 				<div>
 					<strong>Metroidvania Compendium</strong>
 					<span> » {breadcrumb}</span>
@@ -26,7 +28,7 @@ const Header: React.FC<HeaderProps> = props => {
 					<Link to="/config">Config</Link>
 					<span> </span>
 				</div>
-			</nav>
+			</Container>
 			<hr></hr>
 		</>
 	);
@@ -38,3 +40,27 @@ const mapStateToProps = (state: AppStore): HeaderProps => {
 	};
 };
 export default connect(mapStateToProps)(Header);
+
+const Container = styled.nav`
+	top: 0;
+	left: 0;
+	right: 0;
+	position: fixed;
+	height: ${Styles.headerHeight};
+	align-items: center;
+	display: flex;
+	justify-content: space-between;
+	padding: 10px;
+	box-sizing: border-box;
+	background: ${Styles.colorDarkBg};
+	color: ${Styles.colorTextOnDarkBg};
+	a {
+		color: white;
+		display: inline-block;
+		padding: 0 10px;
+		text-decoration: none;
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+`;
