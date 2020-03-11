86907abdf5bd1a4be83fb848bd2502a598d42dfa
Michal Obirek <obirek999@gmail.com>
2020-03-11 13:37:13 +0100

    Styling of HomeView

--- frontend/components/styles.ts
+++ frontend/components/styles.ts
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 export const Styles = {
 	colorAccentBg: '#F8F8F8',
 	colorAccentDarkBg: '#E8E8E8',
@@ -11,3 +13,7 @@ export const Styles = {
 
 	sectionHeaderSize: '26px',
 };
+export const Narrow = styled.div`
+	width: 800px;
+	margin: 0 auto;
+`;
--- frontend/views/HomeView.tsx
+++ frontend/views/HomeView.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-// eslint-disable-next-line no-unused-vars
 import { Store } from 'redux';
-// eslint-disable-next-line no-unused-vars
 import { AppStore, AppActions, BreadcrumbActions } from '../storage/common';
+import styled from 'styled-components';
+import { Narrow } from '../components/styles';
+
+const Box = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+`;
+
+const Title = styled.h1`
+	font-size: 5vw;
+	text-align: center;
+	text-decoration: underline;
+`;
+
+const Description = styled.article`
+	font-size: 24px;
+	text-align: justify;
+`;
 
 interface HomeViewProps {
 	store: Store<AppStore, AppActions>;
@@ -15,10 +32,10 @@ export class HomeView extends React.Component<HomeViewProps> {
 
 	public render() {
 		return (
-			<>
-				<h1>Metroidvania Compendium</h1>
-				<div className="container">
-					<article>
+			<Box>
+				<Title>Metroidvania Compendium</Title>
+				<Narrow>
+					<Description>
 						<p>
 							A comprehensive compendium of abilities in Metroidvania games. Perfect research material for your own game. Excellent if you want to
 							learn more about the genre you love. Invaluable source of inspiration for any kind of game.
@@ -27,9 +44,9 @@ export class HomeView extends React.Component<HomeViewProps> {
 						<p>
 							<Link to="/config">Configure your experience</Link> to avoid them.
 						</p>
-					</article>
-				</div>
-			</>
+					</Description>
+				</Narrow>
+			</Box>
 		);
 	}
 }
