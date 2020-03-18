8aab61eaa7c4bd2f22887b685931cf0f49951850
Michal Obirek <obirek999@gmail.com>
2020-03-18 12:13:05 +0100

    Destructuring at the beginning

--- frontend/storage/utils/createGameAbilitiesListData.ts
+++ frontend/storage/utils/createGameAbilitiesListData.ts
@@ -12,27 +12,26 @@ interface GameAbilitiesListInitialProps {
 	gameId: number;
 }
 export function createGameAbilitiesListData(props: GameAbilitiesListInitialProps): GameAbilitiesListProps {
+	const { abilities, abilityExamples, abilityGroups, abilityCategories, gameId } = props;
 	const gameAbilitiesListData: GameAbilitiesListProps = { abilities: new Map(), categories: new Array() };
-	const abilityExamplesThisGame = props.abilityExamples.filter((abilityExample: AbilityExample) => {
-		return abilityExample.gameId == props.gameId;
+	const abilityExamplesThisGame = abilityExamples.filter((abilityExample: AbilityExample) => {
+		return abilityExample.gameId == gameId;
 	});
 	for (const abilityExample of abilityExamplesThisGame) {
-		const ability = props.abilities.get(abilityExample.abilityId)!;
-		const group = props.abilityGroups.get(ability.groupId)!;
-		const category = props.abilityCategories.get(group.categoryId)!;
+		const ability = abilities.get(abilityExample.abilityId)!;
+		const group = abilityGroups.get(ability.groupId)!;
+		const category = abilityCategories.get(group.categoryId)!;
 		if (!gameAbilitiesListData.abilities.has(category.id)) {
 			gameAbilitiesListData.abilities.set(category.id, new Array());
 			gameAbilitiesListData.categories.push(category);
 		}
-		gameAbilitiesListData.abilities
-			.get(category.id)!
-			.push({
-				abilityId: ability.id,
-				abilityName: ability.name,
-				description: ability.description,
-				exampleId: abilityExample.id,
-				exampleName: abilityExample.name,
-			});
+		gameAbilitiesListData.abilities.get(category.id)!.push({
+			abilityId: ability.id,
+			abilityName: ability.name,
+			description: ability.description,
+			exampleId: abilityExample.id,
+			exampleName: abilityExample.name,
+		});
 	}
 	return gameAbilitiesListData;
 }
