cc6ff7ef9103f6a7044e9f812f195e1ce713d1f6
Michal Obirek <obirek999@gmail.com>
2020-03-21 12:04:28 +0100

    Fixed naming issue

--- frontend/routes/AbilitiesRoute.tsx
+++ frontend/routes/AbilitiesRoute.tsx
@@ -4,11 +4,11 @@ import { connect } from 'react-redux';
 import { Dispatch, AnyAction } from 'redux';
 import { AbilitiesView } from '../views/AbilitiesView';
 
-interface HomeRouteProps {
+interface AbilitiesRouteProps {
 	dispatch: Dispatch<AnyAction>;
 }
 
-export class HomeRoute extends React.Component<HomeRouteProps> {
+export class AbilitiesRoute extends React.Component<AbilitiesRouteProps> {
 	public componentDidMount() {
 		this.props.dispatch(BreadcrumbActions.setBreadcrumb('Abilities'));
 	}
@@ -16,8 +16,8 @@ export class HomeRoute extends React.Component<HomeRouteProps> {
 		return <AbilitiesView />;
 	}
 }
-const mapStateToProps = (): Partial<HomeRouteProps> => {
+const mapStateToProps = (): Partial<AbilitiesRouteProps> => {
 	return {};
 };
 
-export default connect(mapStateToProps)(HomeRoute);
+export default connect(mapStateToProps)(AbilitiesRoute);
