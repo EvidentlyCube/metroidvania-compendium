cf8c9622959daee2de9b32395999413e5de97b63
Michal Obirek <obirek999@gmail.com>
2020-03-24 23:51:29 +0100

    Deleted multiApiRequestGet Changed data returned by function Added checks for type of response

--- frontend/storage/utils/databaseRequestManager.ts
+++ frontend/storage/utils/databaseRequestManager.ts
@@ -1,7 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { SERVER_ADRESS } from '../common';
 
-export async function singleDatabaseRequest(endpoint: string, queryParams: Map<string, any>) {
+export async function singleApiRequestGet(endpoint: string, queryParams: Map<string, any>) {
+	const endpointFullString = fullEndpointAddressCreator(endpoint, queryParams);
+	try {
+		const response = await axios.get(endpointFullString);
+		if (typeof response.data === 'object' && response.data.error === null) {
+			return response.data.data;
+		} else {
+			throw new Error('Invalid structure of the response. Response acquired from the server: ' + response);
+		}
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+}
+
+function fullEndpointAddressCreator(endpoint: string, queryParams: Map<string, any>): string {
 	let endpointFullString = SERVER_ADRESS + endpoint;
 	if (queryParams.size !== 0) {
 		endpointFullString += '?';
@@ -9,31 +24,5 @@ export async function singleDatabaseRequest(endpoint: string, queryParams: Map<s
 			endpointFullString += key + '=' + value + '&';
 		}
 	}
-	const response = await axios.get(endpointFullString).catch(function(error) {
-		console.log(error);
-		return null;
-	});
-	if (response) {
-		return response;
-	}
-}
-export async function multiDatabaseRequest(endpointsArray: Array<String>, queryParamsArray: Array<Map<string, any>>) {
-	const requestsArray: Promise<AxiosResponse<any>>[] = [];
-	for (let i = 0; i < endpointsArray.length; i++) {
-		let endpointFullString = SERVER_ADRESS + endpointsArray[i];
-		if (queryParamsArray[i].size !== 0) {
-			endpointFullString += '?';
-			for (const [key, value] of queryParamsArray[i].entries()) {
-				endpointFullString += key + '=' + value + '&';
-			}
-		}
-		requestsArray.push(axios.get(endpointFullString));
-	}
-	const responseArray = await axios.all(requestsArray).catch(function(error) {
-		console.log(error);
-		return null;
-	});
-	if (responseArray) {
-		return responseArray;
-	}
+	return endpointFullString;
 }
