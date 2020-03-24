1456dafb3ac6948aff2874cdab08cab19edb5e04
Michal Obirek <obirek999@gmail.com>
2020-03-24 23:27:21 +0100

    Changed wrapper function to endpoint + params

--- frontend/storage/utils/databaseRequestManager.ts
+++ frontend/storage/utils/databaseRequestManager.ts
@@ -1,8 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import { SERVER_ADRESS } from '../common';
 
-export async function singleDatabaseRequest(endpointDetails: string) {
-	const response = await axios.get(SERVER_ADRESS + endpointDetails).catch(function(error) {
+export async function singleDatabaseRequest(endpoint: string, queryParams: Map<string, any>) {
+	let endpointFullString = SERVER_ADRESS + endpoint;
+	if (queryParams.size !== 0) {
+		endpointFullString += '?';
+		for (const [key, value] of queryParams.entries()) {
+			endpointFullString += key + '=' + value + '&';
+		}
+	}
+	const response = await axios.get(endpointFullString).catch(function(error) {
 		console.log(error);
 		return null;
 	});
@@ -10,10 +17,17 @@ export async function singleDatabaseRequest(endpointDetails: string) {
 		return response;
 	}
 }
-export async function multiDatabaseRequest(endpointDetailsArray: Array<String>) {
+export async function multiDatabaseRequest(endpointsArray: Array<String>, queryParamsArray: Array<Map<string, any>>) {
 	const requestsArray: Promise<AxiosResponse<any>>[] = [];
-	for (const endpointDetails of endpointDetailsArray) {
-		requestsArray.push(axios.get(SERVER_ADRESS + endpointDetails));
+	for (let i = 0; i < endpointsArray.length; i++) {
+		let endpointFullString = SERVER_ADRESS + endpointsArray[i];
+		if (queryParamsArray[i].size !== 0) {
+			endpointFullString += '?';
+			for (const [key, value] of queryParamsArray[i].entries()) {
+				endpointFullString += key + '=' + value + '&';
+			}
+		}
+		requestsArray.push(axios.get(endpointFullString));
 	}
 	const responseArray = await axios.all(requestsArray).catch(function(error) {
 		console.log(error);
