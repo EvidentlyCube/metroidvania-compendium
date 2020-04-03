3d581998ba2ae16ef9842781add0fdc1bd614ea3
Maurycy Zarzycki <maurycy@evidentlycube.com>
2020-04-03 10:06:10 +0200

    Fixed endpoint requests that query by multiple IDs throwing and exception

--- backend/database/TypeormDatabase.ts
+++ backend/database/TypeormDatabase.ts
@@ -1,5 +1,5 @@
 import { ObjectType } from 'typeorm/common/ObjectType';
-import { Connection } from 'typeorm';
+import { Connection, In } from 'typeorm';
 import { Database, EntityConditions } from './Database';
 
 export class TypeormDatabase implements Database {
@@ -14,6 +14,18 @@ export class TypeormDatabase implements Database {
 	}
 
 	public async findManyBy<Entity>(entity: ObjectType<Entity>, conditions: EntityConditions<Entity>): Promise<Entity[]> {
+		for (const field in conditions) {
+			if (!Object.prototype.hasOwnProperty.call(conditions, field)) {
+				continue;
+			}
+
+			const condition = conditions[field];
+
+			if (Array.isArray(condition)) {
+				conditions[field] = condition.length === 1 ? condition[0] : In(condition);
+			}
+		}
+
 		return this._connection.getRepository(entity).find({ where: conditions });
 	}
 
